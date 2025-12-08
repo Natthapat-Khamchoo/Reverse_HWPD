6209da3ad5c0cb53fe977a70c11202aae02a9afb
Natthapat Khamchoo <marsocas.1998@gmail.com>
2025-12-08 23:00:53 +0700

    Fix package.json and vite config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-      },
-    },
+  server: {
+    port: 3000, // กำหนด port เริ่มต้น (เปลี่ยนได้ตามต้องการ)
+    open: true, // เปิด browser อัตโนมัติเมื่อสั่ง run
   },
+  build: {
+    outDir: 'dist',
+  }
 })
